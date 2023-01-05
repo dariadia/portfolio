0f2c90ff54c0435a60c764d4267cd4cc90443517
Daria <daria.diachkova@mail.ru>
2023-01-05 15:33:41 +0100

    chore(app): style

--- src/components/icon.js
+++ src/components/icon.js
@@ -1,26 +1,26 @@
-import React from 'react'
+import React from 'react';
 
 export const Icon = ({ name }) => {
   switch (name) {
     case 'Bookmark':
-      return <FontAwesomeIcon icon="fa-regular fa-circle-bookmark" />
+      return <FontAwesomeIcon icon="fa-regular fa-circle-bookmark" />;
     case 'Codepen':
-      return <FontAwesomeIcon icon="fa-brands fa-codepen" />
+      return <FontAwesomeIcon icon="fa-brands fa-codepen" />;
     case 'External':
-      return <FontAwesomeIcon icon="fa-solid fa-arrow-up-right-from-square" />
+      return <FontAwesomeIcon icon="fa-solid fa-arrow-up-right-from-square" />;
     case 'Folder':
-      return <FontAwesomeIcon icon="fa-regular fa-folder-bookmark" />
+      return <FontAwesomeIcon icon="fa-regular fa-folder-bookmark" />;
     case 'GitHub':
-      return <FontAwesomeIcon icon="fa-brands fa-github" />
+      return <FontAwesomeIcon icon="fa-brands fa-github" />;
     case 'Linkedin':
-      return <FontAwesomeIcon icon="fa-brands fa-linkedin-in" />
+      return <FontAwesomeIcon icon="fa-brands fa-linkedin-in" />;
     case 'Loader':
-      return <FontAwesomeIcon icon="fa-solid fa-loader" />
+      return <FontAwesomeIcon icon="fa-solid fa-loader" />;
     case 'ArrowUp':
-      return <FontAwesomeIcon icon="fa-regular fa-circle-arrow-up" />
+      return <FontAwesomeIcon icon="fa-regular fa-circle-arrow-up" />;
     case 'Telegram':
-      return <FontAwesomeIcon icon="fa-brands fa-telegram" />
+      return <FontAwesomeIcon icon="fa-brands fa-telegram" />;
     default:
-      return <FontAwesomeIcon icon="fa-solid fa-arrow-up-right-from-square" />
+      return <FontAwesomeIcon icon="fa-solid fa-arrow-up-right-from-square" />;
   }
 }
--- src/styles/fonts.js
+++ src/styles/fonts.js
@@ -1,44 +1,42 @@
-import { css } from 'styled-components'
-
-import LibreBaskervilleRegularSrc from '@fonts/LibreBaskerville/LibreBaskerville-Regular.ttf'
-import LibreBaskervilleBoldSrc from '@fonts/LibreBaskerville/LibreBaskerville-Bold.ttf'
-import LibreBaskervilleItalicSrc from '@fonts/LibreBaskerville/LibreBaskerville-Italic.ttf'
-
-import WorkSansItalicSrc from '@fonts/WorkSans/WorkSans-Italic.ttf'
-import WorkSansRegularSrc from '@fonts/WorkSans/WorkSans.ttf'
+import { css } from 'styled-components';
+import LibreBaskervilleRegularSrc from '@fonts/LibreBaskerville/LibreBaskerville-Regular.ttf';
+import LibreBaskervilleBoldSrc from '@fonts/LibreBaskerville/LibreBaskerville-Bold.ttf';
+import LibreBaskervilleItalicSrc from '@fonts/LibreBaskerville/LibreBaskerville-Italic.ttf';
+import WorkSansItalicSrc from '@fonts/WorkSans/WorkSans-Italic.ttf';
+import WorkSansRegularSrc from '@fonts/WorkSans/WorkSans.ttf';
 
 const LibreBaskervilleNormal = {
   400: LibreBaskervilleRegularSrc,
   600: LibreBaskervilleBoldSrc,
-}
+};
 
 const LibreBaskervilleItalic = {
   400: LibreBaskervilleItalicSrc,
-}
+};
 
 const LibreBaskerville = {
   name: 'Libre Baskerville',
   normal: LibreBaskervilleNormal,
   italic: LibreBaskervilleItalic,
-}
+};
 
 const WorkSansNormal = {
   400: WorkSansRegularSrc,
   600: WorkSansRegularSrc,
-}
+};
 
 const WorkSansItalic = {
   400: WorkSansItalicSrc,
-}
+};
 
 const WorkSans = {
   name: 'Work Sans',
   normal: WorkSansNormal,
   italic: WorkSansItalic,
-}
+};
 
 const createFontFaces = (family, style = 'normal') => {
-  let styles = ''
+  let styles = '';
 
   for (const [weight, src] of Object.entries(family[style])) {
     styles += `
@@ -50,19 +48,19 @@ const createFontFaces = (family, style = 'normal') => {
         font-display: auto;
       }
     `;
-  }
+  };
 
-  return styles
+  return styles;
 }
 
-const LibreBaskervilleFont = createFontFaces(LibreBaskerville)
-const LibreBaskervilleItalicFont = createFontFaces(LibreBaskerville, 'italic')
+const LibreBaskervilleFont = createFontFaces(LibreBaskerville);
+const LibreBaskervilleItalicFont = createFontFaces(LibreBaskerville, 'italic');
 
-const WorkSansFont = createFontFaces(WorkSans)
-const WorkSansItalicFont = createFontFaces(WorkSans, 'italic')
+const WorkSansFont = createFontFaces(WorkSans);
+const WorkSansItalicFont = createFontFaces(WorkSans, 'italic');
 
 const Fonts = css`
   ${LibreBaskervilleFont + LibreBaskervilleItalicFont + WorkSansFont + WorkSansItalicFont}
-`
+`;
 
-export default Fonts
+export default Fonts;
