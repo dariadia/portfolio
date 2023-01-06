3e0e01ef6e6682e9b32d602b639ce5fb479fe2cd
Daria <daria.diachkova@mail.ru>
2023-01-06 10:05:32 +0100

    fix(icons): className

--- src/components/icon.js
+++ src/components/icon.js
@@ -3,23 +3,23 @@ import React from 'react';
 const Icon = ({ name }) => {
   switch (name) {
     case 'Bookmark':
-      return <i class="fa-regular fa-circle-bookmark" />;
+      return <i className="fa-regular fa-circle-bookmark" />;
     case 'Codepen':
-      return <i class="fa-brands fa-codepen" />;
+      return <i className="fa-brands fa-codepen" />;
     case 'External':
-      return <i class="fa-solid fa-arrow-up-right-from-square" />;
+      return <i className="fa-solid fa-arrow-up-right-from-square" />;
     case 'Folder':
-      return <i class="fa-regular fa-folder-bookmark" />;
+      return <i className="fa-regular fa-folder-bookmark" />;
     case 'GitHub':
-      return <i class="fa-brands fa-github"></i>;
+      return <i className="fa-brands fa-github"></i>;
     case 'Linkedin':
-      return <i class="fa-brands fa-linkedin-in" />;
+      return <i className="fa-brands fa-linkedin-in" />;
     case 'ArrowUp':
-      return <i class="fa-solid fa-circle-arrow-up" />;
+      return <i className="fa-solid fa-circle-arrow-up" />;
     case 'Telegram':
-      return <i class="fa-brands fa-telegram" />;
+      return <i className="fa-brands fa-telegram" />;
     default:
-      return <i class="fa-solid fa-arrow-up-right-from-squar" />;
+      return <i className="fa-solid fa-arrow-up-right-from-squar" />;
   }
 }
 
