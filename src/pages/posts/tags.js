e7e85dbe222290caf9c066e06bf3bb6b65d38431
Daria <daria.diachkova@mail.ru>
2023-01-07 13:27:09 +0100

    feat(components): add tags

---
+++ src/pages/posts/tags.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import { Link, graphql } from 'gatsby';
+import kebabCase from 'lodash/kebabCase';
+
+import { Helmet } from 'react-helmet';
+import styled from 'styled-components';
+import { Layout } from '@components';
+
+const StyledTagsContainer = styled.main`
+  max-width: 1000px;
+
+  h1 {
+    margin-bottom: 50px;
+  }
+  ul {
+    color: var(--light-slate);
+
+    li {
+      font-size: var(--xxl);
+
+      a {
+        color: var(--light-slate);
+
+        .count {
+          color: var(--slate);
+          font-family: var(--font-main);
+          font-size: var(--md);
+        }
+      }
+    }
+  }
+`;
+
+const TagsPage = ({
+  data: {
+    allMarkdownRemark: { group },
+  },
+  location,
+}) => (
+  <Layout location={location}>
+    <Helmet title="Tags" />
+    <StyledTagsContainer>
+      <span className="breadcrumb">
+        <span className="arrow">&larr;</span>
+        <Link to="/pensieve">All memories</Link>
+      </span>
+      <h1>Tags</h1>
+      <ul className="fancy-list">
+        {group.map(tag => (
+          <li key={tag.fieldValue}>
+            <Link to={`/pensieve/tags/${kebabCase(tag.fieldValue)}/`} className="inline-link">
+              {tag.fieldValue} <span className="count">({tag.totalCount})</span>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </StyledTagsContainer>
+  </Layout>
+);
+
+export default TagsPage;
+
+export const pageQuery = graphql`
+  query {
+    allMarkdownRemark(limit: 2000, filter: { frontmatter: { draft: { ne: true } } }) {
+      group(field: frontmatter___tags) {
+        fieldValue
+        totalCount
+      }
+    }
+  }
+`;
